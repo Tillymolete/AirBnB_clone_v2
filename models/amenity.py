5e42b563afaa44b7458c83f527dee962e61e0711
Tillymolete <mahlatsemolete@gmail.com>
2024-01-15 10:33:31 -0800

    amenityclass

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""class for amenity for HBNB project """
+import models
 from models.base_model import BaseModel
-
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
-    name = ""
+    """representation of class amenity"""
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialises amenity class"""
+        super().__init__(*args, **kwargs)
