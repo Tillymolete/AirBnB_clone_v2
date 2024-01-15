eb014a5a974de7031b621eb5d2e9c58a86911a86
DouibaOmar <douibaomar40@gmail.com>
2024-01-15 18:53:32 +0100

    Done

--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,10 @@ from models.place import Place
 from models.review import Review
 from os import getenv
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
+storage_type = getenv('HBNB_TYPE_STORAGE')
+if storage_type == 'db':
     from models.engine.db_storage import DBStorage
-    storage = File Storage()
+    storage = FileStorage()
 
 else:
     from models.engine.file_storage import FileStorage
